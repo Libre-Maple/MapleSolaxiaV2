308ad30f97ce4a10b2181613a77f8e6e3733bd88
Spiral <krist10452@yahoo.com>
2017-11-02 17:53:06 -0400

    Update 21302.js

--- scripts/quest/21302.js
+++ scripts/quest/21302.js
@@ -3,19 +3,16 @@
     Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
 		       Matthias Butz <matze@odinms.de>
 		       Jan Christian Meyer <vimes@odinms.de>
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
     published by the Free Software Foundation version 3 as published by
     the Free Software Foundation. You may not use, modify or distribute
     this program under any other version of the GNU Affero General Public
     License.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -44,10 +41,10 @@ function end(mode, type, selection) {
 			}
 			
 			qm.gainItem(1142131, true);
-			qm.getPlayer().setMaxMp(qm.getPlayer().getMaxMp() + 275);
-			qm.getPlayer().setMaxHp(qm.getPlayer().getMaxHp() + 275);
+                        
 			qm.changeJobById(2111);
 			qm.teachSkill(21110002, 0, 20, -1);
+                        
 			qm.completeQuest();
 		}
 		qm.sendNext("Come on, keep training so you can get all your abilities back, and that way we can explore together once more!");    
